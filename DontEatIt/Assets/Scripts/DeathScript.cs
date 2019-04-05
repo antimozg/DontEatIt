e153630f773ba6b48c33b6f701b73366134bdfda
pavel9609 <pavel9609@gmail.com>
2019-04-05 10:14:08 +0300

    Изменение счета

--- DontEatIt/Assets/Scripts/DeathScript.cs
+++ DontEatIt/Assets/Scripts/DeathScript.cs
@@ -8,7 +8,8 @@ public class DeathScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Text scoring = GetComponent<Text>();
+        scoring.transform.position = new Vector3(Screen.currentResolution.width - 1000.0f, Screen.currentResolution.height - 50.0f);
     }
 
     // Update is called once per frame
--- DontEatIt/Assets/Scripts/ScoreOutput.cs
+++ DontEatIt/Assets/Scripts/ScoreOutput.cs
@@ -7,14 +7,14 @@ public class ScoreOutput : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Text scoring = GetComponent<Text>();
+        scoring.transform.position = new Vector3(Screen.currentResolution.width-200.0f, Screen.currentResolution.height - 50.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
         Text scoring = GetComponent<Text>();
-        Debug.Log(Scores.score);
         scoring.text = "Score: "+ Scores.score;
     }
 }
